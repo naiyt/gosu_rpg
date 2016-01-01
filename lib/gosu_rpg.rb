3e61292ec7a238c42f1eebbe9ce3b3ec2a71c686
Nate Collings <nate@natecollings.com>
2016-01-01 12:45:49 -0700

    Add config ability

--- lib/gosu_rpg.rb
+++ lib/gosu_rpg.rb
@@ -1,5 +1,20 @@
 require 'gosu'
+require 'gosu_rpg/config'
 require 'gosu_rpg/version'
 require 'gosu_rpg/exceptions'
 require 'gosu_rpg/game_window'
 require 'gosu_rpg/game_state'
+
+module GosuRPG
+  def self.configure
+    yield config
+  end
+
+  def self.config
+    @config || Config.new
+  end
+
+  def self.reset
+    @config = Config.new
+  end
+end
---
+++ lib/gosu_rpg/config.rb
@@ -0,0 +1,5 @@
+module GosuRPG
+  class Config
+    attr_accessor :w_width, :w_height, :default_message
+  end
+end
