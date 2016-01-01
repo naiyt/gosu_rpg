05d6416e9036df8d8e0011b6c9aef778bf1a7c60
Nate Collings <nate@natecollings.com>
2016-01-01 13:21:52 -0700

    A second sample state

--- sample_game/sample_game.rb
+++ sample_game/sample_game.rb
@@ -15,6 +15,29 @@ class SampleState < GosuRPG::GameState
   def draw
     @font.draw('A cool menu state!', 10, 10, 0)
   end
+
+  def button_down(id)
+    GosuRPG::GameState.switch(AnotherState.new) if id == Gosu::Kb2
+  end
+end
+
+class AnotherState < GosuRPG::GameState
+  def initialize
+    super
+
+    @window.caption = 'Whoa, a second state!'
+  end
+
+  def update
+  end
+
+  def draw
+    @font.draw('This state is even better!!', 20, 20, 0)
+  end
+
+  def button_down(id)
+    GosuRPG::GameState.switch(SampleState.new) if id == Gosu::Kb1
+  end
 end
 
 GosuRPG.configure do |config|
