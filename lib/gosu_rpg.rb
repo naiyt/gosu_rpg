744c95eb25da6d90b30821c02ff71cd6fb0bd151
Nate Collings <nate@natecollings.com>
2016-01-01 12:17:17 -0700

    Flesh out the base Window class a bit

--- lib/gosu_rpg.rb
+++ lib/gosu_rpg.rb
@@ -1,3 +1,4 @@
+require 'gosu'
 require 'gosu_rpg/version'
-require 'gosu_rpg/game_window'
 require 'gosu_rpg/exceptions'
+require 'gosu_rpg/game_window'
--- lib/gosu_rpg/game_window.rb
+++ lib/gosu_rpg/game_window.rb
@@ -1,15 +1,15 @@
 module GosuRPG
   class GameWindow < Gosu::Window
-    def initialize(w_height, w_width)
-      super W_WIDTH, W_HEIGHT
+    def initialize(w_width, w_height)
+      super w_width, w_height
     end
 
     def update
-      raise NotImplemented('Your Window class must implement the update method!')
+      raise NotImplemented, 'Your Window class must implement the update method!'
     end
 
     def draw
-      raise NotImplemented('Your Window class must implement the draw method!')
+      raise NotImplemented, 'Your Window class must implement the draw method!'
     end
   end
 end
